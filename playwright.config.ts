06e65cbb1ba7981d4985a4b0489bac55fbcb0532
Philine Hoernemann <phhoernemann@web.de>
2024-06-10 16:22:00 +0200

    update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,6 @@
 import type { PlaywrightTestConfig } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173,
-	},
 	use: {
 		baseURL: process.env.BASE_URL || 'http://localhost:4173',
 	},
